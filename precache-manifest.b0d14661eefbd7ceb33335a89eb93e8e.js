25133eb5d1a0438c103075c2b420c900f33321e4
AugustoBarco <45525340+AugustoBarco@users.noreply.github.com>
2019-05-12 19:45:33 -0400

    Add files via upload

---
+++ precache-manifest.b0d14661eefbd7ceb33335a89eb93e8e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1714c64780778612354f7856fdd4a8b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64d7d86433b232bc2499",
+    "url": "/static/css/main.c98a9a9d.chunk.css"
+  },
+  {
+    "revision": "5a74f477ea4b95092f11",
+    "url": "/static/js/2.f818ef5c.chunk.js"
+  },
+  {
+    "revision": "64d7d86433b232bc2499",
+    "url": "/static/js/main.c6e38d49.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7931fab3367fdaea0b02735927abfb43",
+    "url": "/static/media/Digital-7.ttf.7931fab3.eot"
+  },
+  {
+    "revision": "b1ef7eb7f336cbf7f74c1941266ab844",
+    "url": "/static/media/Digital-7.ttf.b1ef7eb7.svg"
+  },
+  {
+    "revision": "d1a0a8cb741b73eb2259828eda5302ca",
+    "url": "/static/media/Digital-7.ttf.d1a0a8cb.woff"
+  },
+  {
+    "revision": "0ba2aa20e2c2ce80e9a2db5b07198464",
+    "url": "/static/media/Octocat.0ba2aa20.png"
+  },
+  {
+    "revision": "519430acdb7c4e92c22be099cb2b4450",
+    "url": "/static/media/firma.519430ac.png"
+  },
+  {
+    "revision": "eb4515d77e2bfd53530026c46c251791",
+    "url": "/static/media/love.eb4515d7.png"
+  },
+  {
+    "revision": "47ce6e77f039020ee2e76a10c1e988e9",
+    "url": "/static/media/react.47ce6e77.png"
+  }
+]);
